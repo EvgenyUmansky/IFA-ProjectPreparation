659dc4d3daa3998fe275b8f2938616d87042a251
pokemon98 <55584213+pokemon98@users.noreply.github.com>
2020-04-07 18:03:35 +0300

    Season + League Constructors

--- src/main/java/domain/League.java
+++ src/main/java/domain/League.java
@@ -1,9 +1,24 @@
 package domain;
 
+import service.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class League {
+    private Controller controller;
+    private List<Season> leagueSeasons;
+    //TODO: check for other variables
+
+    public League(Controller controller, List<Season> leagueSeasons) {
+        this.controller = controller;
+        this.leagueSeasons = leagueSeasons;
+    }
 
     // Constructor
     public League() {
+        this.controller = new Controller();
+        this.leagueSeasons = new ArrayList<Season>();
 
     }
 }
--- src/main/java/domain/Season.java
+++ src/main/java/domain/Season.java
@@ -1,9 +1,22 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Season {
+    List<Game> seasonGames;
+    List<Team> seasonTeams;
+    int year;
+
+    public Season(List<Game> seasonGames, List<Team> seasonTeams, int year) {
+        this.seasonGames = seasonGames;
+        this.seasonTeams = seasonTeams;
+        this.year = year;
+    }
 
     // Constructor
     public Season() {
-
+        this.seasonGames = new ArrayList<Game>();
+        this.seasonTeams = new ArrayList<Team>();
     }
 }
