02111d66059395b5c931fd5ce98846d3d5fc91a6
Evgeniy Umanskiy <evgeniyu@sensus.co.il>
2020-04-09 00:30:22 +0300

    SystemAdministrator.java: + Constructor

--- src/main/java/domain/SystemAdministrator.java
+++ src/main/java/domain/SystemAdministrator.java
@@ -1,9 +1,10 @@
 package domain;
 
 public class SystemAdministrator extends Subscriber {
+    Alert alert;
 
-    // Constructor
-    public SystemAdministrator(){
-
+/////////// Constructor ///////////
+    public SystemAdministrator(String userName, String password, String name, String mail) {
+        super(userName, password, name, mail);
     }
 }
