aef57b46394d93eb89ee4dd34f4cc1ca685d1535
Evgeniy Umanskiy <evgeniyu@sensus.co.il>
2020-04-09 00:35:35 +0300

    Subscriber.java: + addAlertMessage(String message) + getters and setters for userName, password, name, mail

--- src/main/java/domain/Subscriber.java
+++ src/main/java/domain/Subscriber.java
@@ -1,19 +1,30 @@
 package domain;
 
+
+import java.util.ArrayList;
+
 public abstract class Subscriber extends User {
 
     private boolean connected;
     private String userName;
     private String password;
     private String name;
+    private String mail;
 
-    // Constructor
-    public Subscriber(String userName, String password, String name){
+    private ArrayList<String> alertsMessages;
+
+/////////// Constructor ///////////
+
+    // With mail
+    public Subscriber(String userName, String password, String name, String mail){
         connected = false;
         this.userName = userName;
         this.name = name;
         this.password = password;
+        this.mail = mail;
+        this.alertsMessages = new ArrayList<>();
     }
+
 
 
     public static boolean isValidUserName(String userName) {
@@ -46,6 +57,12 @@ public abstract class Subscriber extends User {
         return true;
     }
 
+    // when the function activated - send message to screen
+    public String addAlertMessage(String message){
+        this.alertsMessages.add(message);
+
+        return message;
+    }
 
     public boolean isConnected(){
         return connected;
@@ -59,12 +76,40 @@ public abstract class Subscriber extends User {
         connected = false;
     }
 
-    public String getPassword(){
-        return password;
-    }
 
     public String getUserName(){
         return userName;
     }
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public ArrayList<String> getAlertsMessages() {
+        return alertsMessages;
+    }
 }
