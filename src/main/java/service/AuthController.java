a77c0a65a534565ec01f98fee583d45642a2b370
Naor <NaorbenEvgi@gmail.com>
2020-05-19 20:01:37 +0300

    Create login route

--- src/main/java/service/AuthController.java
+++ src/main/java/service/AuthController.java
@@ -1,4 +1,22 @@
 package service;
 
+import domain.User;
+import domain.controllers.StartController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import service.pojos.LoginDTO;
+
+@RestController
 public class AuthController {
+    private StartController controller;
+
+    public AuthController(){
+        controller = new StartController();
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestBody LoginDTO creds) throws Exception {
+        return controller.login(creds.getUsername(), creds.getPassword());
+    }
 }
