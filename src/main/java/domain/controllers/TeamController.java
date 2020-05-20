21fe2d0f4228bc1b7e67586c05822a0ede3d3c16
Naor <NaorbenEvgi@gmail.com>
2020-05-20 13:47:23 +0300

    Auto stash before merge of "Create-api" and "origin/Naor"

--- src/main/java/domain/controllers/TeamController.java
+++ src/main/java/domain/controllers/TeamController.java
@@ -1,6 +1,7 @@
 package domain.controllers;
 
 import domain.*;
+import service.pojos.TeamDTO;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,14 +9,44 @@ import java.util.HashSet;
 
 public class TeamController {
 
-    public ArrayList<Team> getTeams() {
-        // TODO: get all teams from DB
-        return null;
+    public TeamDTO createTeam(String teamOwner, String name, String stadium){
+        User user = User.getUserByID(teamOwner);
+        Field field = Field.getFieldByName(stadium);
+        user.addRoleToUser(Role.TEAM_OWNER);
+        TeamOwner owner = (TeamOwner) user.getRoles().get(Role.TEAM_OWNER);
+        Team newTeam = new Team(name, field, owner);
+        // TODO: Save new team to DB
+        return new TeamDTO(
+                newTeam.getTeamName(),
+                newTeam.getStadium().getFieldName(),
+                newTeam.getFields().keySet().toArray(new String[0]),
+                newTeam.getPlayers().keySet().toArray(new String[0]),
+                newTeam.getCoaches().keySet().toArray(new String[0]),
+                newTeam.getManagers().keySet().toArray(new String[0]),
+                newTeam.getOwners().keySet().toArray(new String[0]),
+                newTeam.getTeamStatus().name()
+        );
+
     }
 
-    public void updateTeam(String teamName, String stadium, HashMap<String,String> fields, HashMap<String, String> players, HashMap<String, String> coaches, HashMap<String, String> managers, HashMap<String, String> owners, String teamStatus){
-        Team team = Team.getTeamByName(teamName);
-        //update the team?
+    public ArrayList<TeamDTO> getTeams() {
+        // TODO: get all teams from DB
+        ArrayList<Team> array = new ArrayList<>();
+        array.add(new Team("test", new Field("Test field", 1), new TeamOwner("user", "user@gmail.com")));
+        ArrayList<TeamDTO> response = new ArrayList<>();
+        for (Team team : array) {
+            response.add(new TeamDTO(
+                    team.getTeamName(),
+                    team.getStadium().getFieldName(),
+                    team.getFields().keySet().toArray(new String[0]),
+                    team.getPlayers().keySet().toArray(new String[0]),
+                    team.getCoaches().keySet().toArray(new String[0]),
+                    team.getManagers().keySet().toArray(new String[0]),
+                    team.getOwners().keySet().toArray(new String[0]),
+                    team.getTeamStatus().name()
+            ));
+        }
+        return response;
     }
 
     // ========================= Guest functions ============================
