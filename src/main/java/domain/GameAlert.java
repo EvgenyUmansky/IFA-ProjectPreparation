eb5ef67a8315ac88e6b69eb161aa7da5e5e92c19
royj1 <royj1708@gmail.com>
2020-05-25 21:48:52 +0300

    more DBAccesses

--- src/main/java/domain/GameAlert.java
+++ src/main/java/domain/GameAlert.java
@@ -4,12 +4,44 @@ package domain;
  * Represents the types of events that can take place during a football match
  */
 public enum GameAlert {
-    GOAL,
-    OFFSIDE,
-    FOUL,
-    RED_CARD,
-    YELLOW_CARD,
-    INJURY,
-    PLAYER_IN,
-    PLAYER_OUT
+    GOAL {
+        public String toString(){
+            return "Goal";
+        }
+    },
+    OFFSIDE {
+        public String toString(){
+            return "Offside";
+        }
+    },
+    FOUL {
+        public String toString(){
+            return "Foul";
+        }
+    },
+    RED_CARD{
+        public String toString(){
+            return "Red_card";
+        }
+    },
+    YELLOW_CARD{
+        public String toString(){
+            return "Yellow_card";
+        }
+    },
+    INJURY{
+        public String toString(){
+            return "Injury";
+        }
+    },
+    PLAYER_IN{
+        public String toString(){
+            return "Player_in";
+        }
+    },
+    PLAYER_OUT{
+        public String toString(){
+            return "Player_out";
+        }
+    }
 }
