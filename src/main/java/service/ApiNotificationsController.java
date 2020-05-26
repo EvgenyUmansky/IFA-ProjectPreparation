3712a8dc2ad38afc6b3f1d4c78b05ccacf0676d4
NaorBenEvgi <naorbenevgi@gmail.com>
2020-05-26 16:56:53 +0300

    the notifications arrive just for the users who followed the game

--- src/main/java/service/ApiNotificationsController.java
+++ src/main/java/service/ApiNotificationsController.java
@@ -1,4 +1,5 @@
 package service;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -8,17 +9,18 @@ import service.model.Notification;
 
 @Controller
 public class ApiNotificationsController {
-    @MessageMapping("/game.register")
-    @SendTo("/topic/public")
-    public Notification register(@Payload Notification notification, SimpMessageHeaderAccessor headerAccessor) {
-        //headerAccessor.getSessionAttributes().put("username", notification.getSender());
-        headerAccessor.setSubscriptionId(notification.getSender());
+    @MessageMapping("/topic/game/register/{gameId}")
+    //@SendTo("/topic/game/{gameId}")
+    public Notification register(@DestinationVariable String gameId, @Payload Notification notification, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", notification.getSender());
+        //headerAccessor.setSubscriptionId(notification.getSender());
         return notification;
     }
 
-    @MessageMapping("/game.send")
-    @SendTo("/topic/public")
-    public Notification sendMessage(@Payload Notification notification) {
+    @MessageMapping("/topic/game/{gameId}")
+    @SendTo("/topic/game/register/{gameId}")
+    public String sendMessage(@Payload String notification) {
+        System.out.println(notification);
         return notification;
     }
 }
--- src/main/java/service/SpringRestApiApplication.java
+++ src/main/java/service/SpringRestApiApplication.java
@@ -3,6 +3,7 @@ package service;
 // To fix any import issue - just add those packages to filepath
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 
 @SpringBootApplication
@@ -11,6 +12,7 @@ public class SpringRestApiApplication {
     public static void main(String[] args) {
         // If we have 'cannot access problem -> go to here: http://programmersought.com/article/48731001282/;jsessionid=25AEE2D3812F22013BECE7C1F03AC5FD
         SpringApplication.run(SpringRestApiApplication.class, args);
+        //System.out.println(SpringVersion.getVersion());
     }
 
     // For more reading on Spring params: https://www.baeldung.com/spring-request-param
