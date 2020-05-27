c00bfdb5b6a5244d39f56a4df277dcc3cf4255d2
Evgeniy Umanskiy <evgeniyu@sensus.co.il>
2020-05-28 01:13:44 +0300

    getGame works

--- src/main/java/service/ApiGameController.java
+++ src/main/java/service/ApiGameController.java
@@ -27,8 +27,8 @@ public class ApiGameController {
     }
 
     @PostMapping("/games/{gameId}")
-    public void addFanSubscriptionToGame(@PathVariable String gameId, @RequestParam String gameMinute, @RequestParam String eventName, @RequestParam String description) throws Exception {
-        controller.addGameEventToGame(gameId, gameMinute, eventName, description);
+    public GameDTO addFanSubscriptionToGame(@PathVariable String gameId, @RequestParam String gameMinute, @RequestParam String eventName, @RequestParam String description) throws Exception {
+        return controller.addGameEventToGame(gameId, gameMinute, eventName, description);
     }
 
     // This will update event by referee user in the following way: /games/{gameId}?eventId=<eventId>
